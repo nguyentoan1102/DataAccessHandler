b93ade20f3ec77a4378134e451478cf467ed25eb
nguyentoan1102 <longtinh12@gmail.com>
2018-05-25 00:18:49 +0700

    DataAccessBase

---
+++ DataAccessHandler/BulkInsertManager.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Reflection;
+
+namespace DataAccessHandler
+{
+    public class BulkInsertManager
+    {
+        public static void SqlBulkCopy(SqlConnection connection, DataTable table, string destinationTable)
+        {
+            connection.Open();
+
+            try
+            {
+                var sqlBulkCopy = new SqlBulkCopy(connection);
+                sqlBulkCopy.DestinationTableName = destinationTable;
+
+                foreach (DataColumn column in table.Columns)
+                {
+                    sqlBulkCopy.ColumnMappings.Add(column.ColumnName, column.ColumnName);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+        public static void SqlBulkCopy<T>(SqlConnection connection, IEnumerable<T> objects, string destinationTable)
+        {
+            try
+            {
+                var sqlBulkCopy = new SqlBulkCopy(connection);
+                sqlBulkCopy.DestinationTableName = destinationTable;
+
+                var table = ConvertToDataTable<T>(objects);
+
+                foreach (DataColumn column in table.Columns)
+                {
+                    sqlBulkCopy.ColumnMappings.Add(column.ColumnName, column.ColumnName);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+        public static DataTable ConvertToDataTable<T>(IEnumerable<T> items)
+        {
+            var tb = new DataTable(typeof(T).Name);
+
+            PropertyInfo[] props = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+            foreach (var prop in props)
+            {
+                tb.Columns.Add(prop.Name, prop.PropertyType);
+            }
+
+            foreach (var item in items)
+            {
+                var values = new object[props.Length];
+                for (var i = 0; i < props.Length; i++)
+                {
+                    values[i] = props[i].GetValue(item, null);
+                }
+
+                tb.Rows.Add(values);
+            }
+
+            return tb;
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using DataAccessHandler;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            List<Author> AuthorList = new List<Author>();
+            AuthorList.Add(new Author("Mahesh Chand", 35, "A Prorammer's Guide to ADO.NET", true, new DateTime(2003, 7, 10)));
+            AuthorList.Add(new Author("Neel Beniwal", 18, "Graphics Development with C#", false, new DateTime(2010, 2, 22)));
+            AuthorList.Add(new Author("Praveen Kumar", 28, "Mastering WCF", true, new DateTime(2012, 01, 01)));
+            AuthorList.Add(new Author("Mahesh Chand", 35, "Graphics Programming with GDI+", true, new DateTime(2008, 01, 20)));
+            AuthorList.Add(new Author("Raj Kumar", 30, "Building Creative Systems", false, new DateTime(2011, 6, 3)));
+            BulkInsertManager.ConvertToDataTable(AuthorList);
+        }
+    }
+}
